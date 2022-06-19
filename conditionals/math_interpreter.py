6ae23da5a1fd3ec99aa0c0be7da8f357697b8cff
Otávio Ligeiro <otavioligeiro0412@gmail.com>
2022-06-19 15:36:17 -0300

    Update math_interpreter.py

--- conditionals/math_interpreter.py
+++ conditionals/math_interpreter.py
@@ -1,15 +1,32 @@
 def main():
   expression = get_expression()
-  members_of_expression = remove_space(expression)
-  print(members_of_expression[1])
+  operators = remove_space(expression)
+  print_result(operators)
 
 def get_expression():
   expression = input("Expression: ")
   return expression
 
 def remove_space(expression):
-  members_of_expression = expression.strip(" ")
-  return members_of_expression
+  operators = expression.split(" ")
+  return operators
+
+def print_result(operators):
+  x = float(operators[0])
+  y = operators[1]
+  z = float(operators[2])
+
+  if (y == "+"):
+    print(f"{x+z}")
+  elif (y == "-"):
+    print(f"{x - z}")
+  elif (y == "*"):
+    print(f"{x * z}")
+  elif (y == "/"):
+    if (z == 0):
+      print("It's not possible to make this operation")
+    else:
+      print(f"{x / z}")
 
 if __name__ == "__main__":
     main()
