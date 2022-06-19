d74cb037c03553094ba01eb16a96d300ccc9fa83
Otávio Ligeiro <otavioligeiro0412@gmail.com>
2022-06-18 22:10:37 -0300

    Create deep_throuth.py

---
+++ conditionals/deep_thought.py
@@ -0,0 +1,20 @@
+def main():
+  answer = get_answer()
+  answer = standardize_answer(answer)
+  verify_answer(answer)
+
+def get_answer():
+  answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+  return answer
+
+def standardize_answer(answer):
+  answer = answer.lower()
+  return answer
+
+def verify_answer(answer):
+  if (answer == "42") or (answer == "forty two") or (answer == "forty-two"):
+    print("Yes")
+  else:
+    print("No")
+
+main()
