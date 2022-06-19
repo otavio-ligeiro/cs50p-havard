c14e32f8dde282cd71e1ce5f72163e46ae2cfe36
Otávio Ligeiro <otavioligeiro0412@gmail.com>
2022-06-19 14:12:48 -0300

    Create math_interpreter.py

---
+++ conditionals/math_interpreter.py
@@ -0,0 +1,19 @@
+def main():
+  continue
+
+def get_expression():
+  expression = input("Expression: ")
+  return expression
+
+def remove_space(expression):
+  expression = expression.replace(" ", "")
+  return expression
+
+
+
+
+
+
+
+
+main()
