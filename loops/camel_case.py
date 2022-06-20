c04e56de5d6d4b472d2b8110267cee41e1f5ec24
Otávio Ligeiro <otavioligeiro0412@gmail.com>
2022-06-20 14:54:44 -0300

    Update camel_case.py

--- loops/camel_case.py
+++ loops/camel_case.py
@@ -1,16 +1,22 @@
 def main():
-  continue
+  string = input_string()
+  convert_snake_case(string)
 
 def input_string():
-  string = input("camelCase:")
+  string = input("camelCase: ")
   return string
 
-def convert_snake_case():
-  
-
-
-
-
+def convert_snake_case(camel_case):
+  snake_case = ""
+  capital_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+  "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  for letter in camel_case:
+    if letter in capital_letters:
+      snake_case += "_"
+      snake_case += letter.lower()
+    else:
+      snake_case += letter
+  print(f"snake_case: {snake_case}")
 
 if __name__ == "__main__":
     main()
